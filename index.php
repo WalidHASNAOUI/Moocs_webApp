023a744160eb1d5e87c9f59342a0690ec941130f
Yassine Tiemicht <tiemicht.yassine@gmail.com>
2022-01-01 17:32:44 +0100

    Search icon improved

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,18 +8,20 @@
     <title>Lister Moocs</title>
     <link rel="stylesheet" href="./css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Encode+Sans+SC:wght@500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+SC:wght@500&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a1987b05f9.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <header>
         <div id="topHeader">
             <div id="left">
                 <p id="pageTitle">LISTER</p>
             </div>
-            <div>
-                <input type="search" name="reperoty" id="serachBox">
+            <div class="searchContainer">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <input type="search" name="reperoty" class=" serachBox" placeholder="Search here">
             </div>
             <div id="right">
                 <img src="./images/profile.png">
@@ -45,22 +48,21 @@
                 </tr>
             </thead>
             <tbody>
-                <?php 
-                    //get all default dir
-                    $defDir = scandir("./Moocs");
-                    
-                    foreach($defDir as $e)
-                    {
-                        echo '
+                <?php
+                //get all default dir
+                $defDir = scandir("./Moocs");
+
+                foreach ($defDir as $e) {
+                    echo '
                             <tr>
                                 <td><i class="fas fa-folder"></i></td>
-                                <td>'.$e.'</td>
-                                <td>'.filetype("./Moocs/".$e).'</td>
-                                <td>'.filesize("./Moocs/".$e).'</td>
-                                <td>'.date("Y-m-d H:i:s a",filemtime("./Moocs/".$e)).'</td>
+                                <td>' . $e . '</td>
+                                <td>' . filetype("./Moocs/" . $e) . '</td>
+                                <td>' . filesize("./Moocs/" . $e) . '</td>
+                                <td>' . date("Y-m-d H:i:s a", filemtime("./Moocs/" . $e)) . '</td>
                             </tr>
                         ';
-                    }
+                }
                 ?>
             </tbody>
         </table>
@@ -70,14 +72,14 @@
     </section>
 
     <aside>
-        <?php 
-            $array = scandir("./Moocs");
-            foreach($array as $e)
-            {
-                echo '<p>'.$e.' --> '.date('Y-m-d G:i:s a',filemtime("./Moocs/".$e)).' --> '.filesize("./Moocs/$e").'B  -->'.filetype("./Moocs/$e").'</p></br>';
-            }
+        <?php
+        $array = scandir("./Moocs");
+        foreach ($array as $e) {
+            echo '<p>' . $e . ' --> ' . date('Y-m-d G:i:s a', filemtime("./Moocs/" . $e)) . ' --> ' . filesize("./Moocs/$e") . 'B  -->' . filetype("./Moocs/$e") . '</p></br>';
+        }
         ?>
     </aside>
     <script src="./js/script.js"></script>
 </body>
+
 </html>
